1485de8e9083832dddeeed5d24718c8a0546eea9
sschoe85 <s@bine-schoetensack.eu>
2020-08-10 11:49:09 +0200

    Search for query

--- src/api/gitHub.js
+++ src/api/gitHub.js
@@ -1,5 +1,5 @@
-export async function getIssues() {
-  const response = await fetch("https://api.github.com/search/issues?q=test");
+export async function getIssues(query) {
+  const response = await fetch("https://api.github.com/search/issues?q=test"+query);
   const result = await response.json();
   return result;
 }
